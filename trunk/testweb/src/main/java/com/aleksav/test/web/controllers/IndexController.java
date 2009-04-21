b577b4c0f494dffe6d660bf742708118c9422023
Aleksa <aleksav@gmail.com>
2009-04-21 17:54:37 +0100

    final today

--- trunk/testweb/src/main/java/com/aleksav/test/web/controllers/IndexController.java
+++ trunk/testweb/src/main/java/com/aleksav/test/web/controllers/IndexController.java
@@ -1,7 +1,21 @@
 package com.aleksav.test.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
 /**
  * @author aleksav
  */
+@Controller
 public class IndexController {
+
+    @RequestMapping("/index.do")
+    public void indexHandler(HttpServletResponse response) throws Exception{
+        PrintWriter writer = response.getWriter();
+        writer.write("Aleksa test");
+        writer.flush();;
+    }
 }
